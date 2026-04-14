8206d89dcae375f8667e468a9d8fc11d4ba5362c
NIDHEESH SN <nidheeshedappon0479@gmail.com>
2026-04-14 08:04:37 +0530

    Import webhook routes in app.js

--- server/src/app.js
+++ server/src/app.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import apiRoutes from "./routes/v1/api.routes.js";
-
+import webhookRoutes from "./routes/v1/webhookRoutes/webhook.routes.js";
 // Load environment variables
 dotenv.config();
 
